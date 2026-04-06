77c5b40afea0078327c27abd984b606b043c7c24
Harry <harrychandra9@gmail.com>
2026-04-06 13:53:36 +0700

    Force remove secrets from tracking

---
+++ WeatherApi/Core/Constant/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  WeatherApi
+//
+//  Created by Kalvin on 06/04/26.
+//
+
+import Foundation
+
+enum Secrets {
+    static let apiKey = "97dfebc5b669ff4bbe7a89f206f318a7"
+}
